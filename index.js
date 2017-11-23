6d2972a1fa036a6488c6acb4130a6d9cd3edb7cd
Your Name <you@example.com>
2017-11-23 11:59:02 +0100

    adding logs

--- index.js
+++ index.js
@@ -28,4 +28,5 @@ app.get('/', function (req, res) {
 
 var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
 var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+console.log("Listening on "+ server_ip_address +", port: " + server_port);
 app.listen(server_port, server_ip_address);
